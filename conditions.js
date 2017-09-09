8b901bdea490c708c51bf83709dcb30a8d0911e2
John Mwashuma <jmwashuma@live.com>
2017-09-09 14:38:24 +0300

    Add how to create js if conditions

---
+++ conditions.js
@@ -0,0 +1,17 @@
+var amAwesome = true;
+
+if (amAwesome){
+    console.log("There is no doubt about that.");
+}
+
+var num = -1;
+
+if(num == 0){
+    console.log("num value is zero");
+}
+else if(num > 0){
+    console.log("num has been assigned the value :" + num);
+}
+else{
+    console.log("num has been assigned a negative value");
+}
